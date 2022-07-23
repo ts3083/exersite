3d32bc6efede3b5adbd6159cfd3de66cae45be10
K_Kwon <ts04031@gmail.com>
2022-07-24 01:30:10 +0900

    첫 프로젝트 생성 + mysql 연동

---
+++ src/main/java/exersite/workout/Member.java
@@ -0,0 +1,19 @@
+package exersite.workout;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Member {
+    
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String name;
+}
