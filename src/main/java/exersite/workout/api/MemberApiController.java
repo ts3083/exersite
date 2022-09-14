7b0e9822cb9401fb5d1bd86e1784eb81f2fb194d
ts3083 <ts04031@gmail.com>
2022-09-14 15:47:40 +0900

    [Create] json 파일 api 통신 - 현재 로그인한 사용자의 정보 조회

---
+++ src/main/java/exersite/workout/api/MemberApiController.java
@@ -0,0 +1,21 @@
+package exersite.workout.api;
+
+import exersite.workout.Config.PrincipalDetails;
+import exersite.workout.Controller.Dtos.MemberDto;
+import exersite.workout.Service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @GetMapping("api/members")
+    public MemberDto profileData(@AuthenticationPrincipal PrincipalDetails details) {
+        return new MemberDto(details);
+    }
+}
