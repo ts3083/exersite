9a53061c3c06542bd4378a8166e3843be6fd5b32
ts3083 <ts04031@gmail.com>
2023-02-07 03:37:09 +0900

    [Update] NotificationInterceptor의 postHandle 조건 처리

--- src/main/java/exersite/workout/Domain/Noticication/NotificationInterceptor.java
+++ src/main/java/exersite/workout/Domain/Noticication/NotificationInterceptor.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,10 +22,15 @@ public class NotificationInterceptor implements HandlerInterceptor {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (modelAndView != null && authentication != null) {
+        if (modelAndView != null && authentication != null && !isRedirectView(modelAndView)
+                && authentication.getPrincipal() instanceof PrincipalDetails) {
             Member member = ((PrincipalDetails) authentication.getPrincipal()).getMember();
             long count = notificationRepository.countByMemberAndChecked(member, false);
             modelAndView.addObject("hasNotification", count > 0);
         }
+    }
+
+    private boolean isRedirectView(ModelAndView modelAndView) {
+        return modelAndView.getViewName().startsWith("redirect:") || modelAndView.getView() instanceof RedirectView;
     }
 }
