c43690cb3244d6593af8188108325e8bd79b8f25
ts3083 <ts04031@gmail.com>
2023-04-03 22:52:02 +0900

    [update] api 개발 security 설정 - 먼저 로그인 과정없이 개발

--- src/main/java/exersite/workout/api/MemberApiController.java
+++ src/main/java/exersite/workout/api/MemberApiController.java
@@ -10,6 +10,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
@@ -17,7 +19,7 @@ public class MemberApiController {
     private final MemberService memberService;
 
     @GetMapping("/api/members")
-    public MemberDto profileData(@CurrentUser Member member) {
-        return new MemberDto(member);
+    public List<MemberDto> profileData() {
+        return memberService.findMemberDtos();
     }
 }
